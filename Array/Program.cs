6e622711d75d6669e65355046cda897516978f5c
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:12:23 +0200

    Add files via upload

---
+++ Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = {"Bmw", "Volvo", "Ford", "Audi", "Tesla"};
+            
+
+            Console.WriteLine(myArray[1]);
+
+            int arrayLength = myArray.Length;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+            foreach (string car in myArray)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
